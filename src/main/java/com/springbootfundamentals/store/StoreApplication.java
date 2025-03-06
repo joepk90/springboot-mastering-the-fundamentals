0d76aa2d162e4e466f7df92e99b5541cceb22850
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-06 20:46:53 +0000

    p1 - 02.04 - setter injection - setup the OrderService class with a setPaymentService method + disable the contructor. update the instantiation of the OrderService to set the PaymentService using the setter method in the StoreApplication file

--- src/main/java/com/springbootfundamentals/store/StoreApplication.java
+++ src/main/java/com/springbootfundamentals/store/StoreApplication.java
@@ -8,7 +8,8 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
-		var OrderService = new OrderService(new PaypalPaymentService());
+		var OrderService = new OrderService();
+		OrderService.setPaymentService(new PaypalPaymentService()); // setter injectin should only be used for optioal dependancies
 		OrderService.placeOrder();
 	}
 
