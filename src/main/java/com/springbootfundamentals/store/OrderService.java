cf307cc8e3a7f9c4ded53d5e3eef3f81b927b5a4
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-06 21:10:03 +0000

    p1 - 02.06 - configuring beans using annotations - add example @Autowired annotation in the OrderService class - commented out

--- src/main/java/com/springbootfundamentals/store/OrderService.java
+++ src/main/java/com/springbootfundamentals/store/OrderService.java
@@ -1,11 +1,17 @@
 package com.springbootfundamentals.store;
 
 import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class OrderService {
     private PaymentService paymentService;
-    
+
+    // @Autowired (only neccessary if the class has multiple contructors)
+    // https://docs.spring.io/spring-framework/reference/core/beans/annotation-config/autowired.html#beans-autowired-annotation-constructor-resolution
+    // public OrderService() {}
+
+    // @Autowired
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
