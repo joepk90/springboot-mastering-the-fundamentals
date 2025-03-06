948889ec6b394f47132d8fc69afc0a9ac5ad2352
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-06 20:47:16 +0000

    Revert "p1 - 02.04 - setter injection - setup the OrderService class with a setPaymentService method + disable the contructor. update the instantiation of the OrderService to set the PaymentService using the setter method in the StoreApplication file"
    This reverts commit f865657ebeda9eef58ba681e503bf487b05e2240.

--- src/main/java/com/springbootfundamentals/store/StoreApplication.java
+++ src/main/java/com/springbootfundamentals/store/StoreApplication.java
@@ -8,8 +8,7 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
-		var OrderService = new OrderService();
-		OrderService.setPaymentService(new PaypalPaymentService()); // setter injectin should only be used for optioal dependancies
+		var OrderService = new OrderService(new PaypalPaymentService());
 		OrderService.placeOrder();
 	}
 
