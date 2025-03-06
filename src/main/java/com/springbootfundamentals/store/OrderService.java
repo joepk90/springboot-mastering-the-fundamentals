8362d5655e2eb0bb0979b0bef14d2400034ab196
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-06 20:59:48 +0000

    p1 - 02.06 - configuring beans using annotations - add the @Component and @Service annotations to the OrderService and PaypalPaymentService classes - to make springboot aware of these classes

--- src/main/java/com/springbootfundamentals/store/OrderService.java
+++ src/main/java/com/springbootfundamentals/store/OrderService.java
@@ -1,5 +1,8 @@
 package com.springbootfundamentals.store;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderService {
     private PaymentService paymentService;
     
