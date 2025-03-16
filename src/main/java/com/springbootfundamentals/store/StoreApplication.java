469cb2350a55a041d6db7b476fd89bf9bf4265fb
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-16 13:10:33 +0000

    p1 - 03.07.02 - simplifying code with lombok - add lombok AllArgsConstructor and NoArgsConstructor annotation to the User entity - demonstrate usage in the StoreApplication class

--- src/main/java/com/springbootfundamentals/store/StoreApplication.java
+++ src/main/java/com/springbootfundamentals/store/StoreApplication.java
@@ -5,11 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.springbootfundamentals.store.entities.User;
+
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		// ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+		/**
+		 * Examples
+		 */
+		// entityAnnotations();
+	}
+
+	public static void entityAnnotations() {
+		var user = new User(1L, "name", "email", "password");
+		user.setName("john");
+		user.setEmail("john@example.com");
+		user.setPassword("password");
 	}
 
 }
--- src/main/java/com/springbootfundamentals/store/entities/User.java
+++ src/main/java/com/springbootfundamentals/store/entities/User.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor // generates a constructor consisting of all the args
+@NoArgsConstructor  // generates a constructor consisting of no args (required because we have overriden the default constructor)
 @Entity
 @Table(name = "users") // custom table name, set here to match our existing table schema
 public class User {
