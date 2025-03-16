9e5aa0e72b77c9d7bf51913705a2e4348a0464b9
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-16 17:43:17 +0000

    p1 - 03.07.07 - creating the category and product entities exercise - update migrations and entities to match course code + add documenting comments relating to the one to many and many to one relationships

--- src/main/java/com/springbootfundamentals/store/entities/Category.java
+++ src/main/java/com/springbootfundamentals/store/entities/Category.java
@@ -1,14 +1,20 @@
 package com.springbootfundamentals.store.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "categories") // custom table name, set here to match our existing table schema
 public class Category {
@@ -21,8 +27,12 @@ public class Category {
     @Column(name = "name")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id") // foreign key
-    private Product product;
+    /**
+     * mappedBy"
+     * tells hy hybernate the category field in the product entity is the owner of the relationship
+     */
+
+    @OneToMany(mappedBy = "category") 
+    private Set<Product> product = new HashSet<>();
 
 }
--- src/main/java/com/springbootfundamentals/store/entities/Product.java
+++ src/main/java/com/springbootfundamentals/store/entities/Product.java
@@ -7,9 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "products") // custom table name, set here to match our existing table schema
 public class Product {
@@ -24,6 +29,11 @@ public class Product {
     @Column(name = "price")
     private BigDecimal price;
 
-    @OneToMany(mappedBy = "category") 
+    /**
+     * Product is the owner of the category/product relationship,
+     * because it has the foreign key which references the category
+     */
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 }
