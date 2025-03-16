ba0c5a1b7836bfaf7a9c4181cb829bed0f7ac278
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-16 12:47:58 +0000

    p1 - 03.07.01 - defining entities - cleanup the existing project - setup the User entity

---
+++ src/main/java/com/springbootfundamentals/store/entities/User.java
@@ -0,0 +1,59 @@
+package com.springbootfundamentals.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users") // custom table name, set here to match our existing table schema
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment the id column
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
