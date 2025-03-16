1e97d645666a395525c0f8e923a228e8136aeaf7
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-16 13:02:34 +0000

    p1 - 03.07.02 - simplifying code with lombok - add lombok Getter and Setter annotation to the User entity - remove unneccesary getters and setters

--- src/main/java/com/springbootfundamentals/store/entities/User.java
+++ src/main/java/com/springbootfundamentals/store/entities/User.java
@@ -6,7 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users") // custom table name, set here to match our existing table schema
 public class User {
@@ -23,37 +27,5 @@ public class User {
 
     @Column(nullable = false, name = "password")
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
