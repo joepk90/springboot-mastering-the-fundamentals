1b4491043a60d01e32127f9393f4a2de0d097415
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-30 16:12:37 +0100

    p1 - 03.08.02 - using repositories - setup new repositoryExamples package - setup usingRepositories method to document saving a user using the UserRepository. get the UserRepository class using springboot context, then create a user object and save it using the UserRepository class

--- src/main/java/com/springbootfundamentals/store/StoreApplication.java
+++ src/main/java/com/springbootfundamentals/store/StoreApplication.java
@@ -3,11 +3,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.springbootfundamentals.repositoryExamples.RepositoryExamples;
+
+
+
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+		var repositoryExamples = new RepositoryExamples(context);
+		repositoryExamples.main();
+		
 	}
 }
