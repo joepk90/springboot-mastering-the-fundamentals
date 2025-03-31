46c48be3a6732a21c4529a17561bf9c844b40f25
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-03-31 21:12:41 +0100

    p1 - 03.08.06 - understanding fetching strategies exercise - add the fetch argument to the ManyToOne annotation on the address entity user field - this prevents the user record being eager loaded along with the profile record

--- src/main/java/com/springbootfundamentals/store/entities/Address.java
+++ src/main/java/com/springbootfundamentals/store/entities/Address.java
@@ -2,6 +2,7 @@ package com.springbootfundamentals.store.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -42,7 +43,7 @@ public class Address {
     @Column(name = "state")
     private String state;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id") // foreign key
     @ToString.Exclude // prevents string conversion cycle in object model (user references addresss -> address references user)
     private User user;
