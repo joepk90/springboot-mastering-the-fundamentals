1df1924ab5453cc2726a615c7607046003d203dd
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-04 20:06:13 +0100

    p1 - 03.09.06 - calling stored procedures - setup createProduct method in the DatabaseSeeder class (custom code - not in course)

--- src/main/java/com/springbootfundamentals/utils/DatabaseSeeder.java
+++ src/main/java/com/springbootfundamentals/utils/DatabaseSeeder.java
@@ -1,16 +1,20 @@
 package com.springbootfundamentals.utils;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
 
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.springbootfundamentals.store.repositories.ProductRepository;
 import com.springbootfundamentals.store.repositories.ProfileRepository;
 import com.springbootfundamentals.store.repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 import com.springbootfundamentals.store.entities.Address;
+import com.springbootfundamentals.store.entities.Category;
+import com.springbootfundamentals.store.entities.Product;
 import com.springbootfundamentals.store.entities.Profile;
 import com.springbootfundamentals.store.entities.Tag;
 import com.springbootfundamentals.store.entities.User;
@@ -66,5 +70,20 @@ public class DatabaseSeeder {
 
 		profileRepository.save(profile);
 		userRepository.save(user);
+	}
+
+	@Transactional
+	public void createProduct() {
+		var productRepository = context.getBean(ProductRepository.class);
+		var category = new Category("Category 2");
+
+        var product = Product.builder()
+        .name("Product 2")
+        .description("Description 2")
+        .price(BigDecimal.valueOf(10.99))
+        .category(category)
+        .build();
+
+        productRepository.save(product);
 	}
 }
